4da20d0d4f6d49446fe29f25ea6ff15ad4ef842c
Lunga Baliwe <lunga001@gmail.com>
2017-03-14 15:57:36 +0200

    LIMS-2579 Added headers on Attachments reports

--- bika/lims/browser/reports/productivity_analysesattachments.py
+++ bika/lims/browser/reports/productivity_analysesattachments.py
@@ -139,8 +139,36 @@ class Report(BrowserView):
                 _('Size'),
                 _('Loaded'),
             ]
-            output = StringIO.StringIO()
-            dw = csv.DictWriter(output, fieldnames=fieldnames)
+            ## Write the report header rows
+            header_output = StringIO.StringIO()
+            writer = csv.writer(header_output)
+            writer.writerow(['Report', 'Attachments'])
+            if 'ClientUID' in self.request.form:
+                writer.writerow(['Client', client_title])
+            writer.writerow([])
+
+            ## Write the parameters used to create the report
+            writer.writerow(['Report parameters:'])
+            writer.writerow([])
+            date_query = formatDateQuery(self.context, 'Loaded')
+            if date_query:
+                string_dates = []
+                for i in date_query['query']:
+                    string_dates.append(
+                            datetime.datetime.strptime(
+                                i, '%Y-%m-%d %H:%M').strftime('%Y-%m-%d'))
+                dates_requested = ' - '.join(string_dates)
+                writer.writerow(['Date Loaded', dates_requested])
+            writer.writerow([])
+
+            ## Write any totals or report statistics
+            writer.writerow(['Total number of analyses:', len(datalines)])
+            writer.writerow([])
+
+            ## Write individual rows to a DictWriter on body_output
+            body_output = StringIO.StringIO()
+            dw = csv.DictWriter(body_output, extrasaction='ignore',
+                                fieldnames=fieldnames)
             dw.writerow(dict((fn, fn) for fn in fieldnames))
             for row in datalines:
                 dw.writerow({'Request': row[0]['value'],
@@ -149,8 +177,10 @@ class Report(BrowserView):
                              'Content type': row[3]['value'],
                              'Size': row[4]['value'],
                              'Loaded': row[5]['value']})
-            report_data = output.getvalue()
-            output.close()
+            report_data = header_output.getvalue() + \
+                          body_output.getvalue()
+            header_output.close()
+            body_output.close()
             date = datetime.datetime.now().strftime("%Y%m%d%H%M")
             setheader = self.request.RESPONSE.setHeader
             setheader('Content-Type', 'text/csv')
