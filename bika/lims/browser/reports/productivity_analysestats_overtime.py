3f55a227f1a4be85a82e88cf27238d8d1ed5f101
Lunga Baliwe <lunga001@gmail.com>
2017-03-23 07:25:42 +0200

    LIMS-2579 Added from and to dates in their own cells in Analysis turnaround time over time report

--- bika/lims/browser/reports/productivity_analysestats_overtime.py
+++ bika/lims/browser/reports/productivity_analysestats_overtime.py
@@ -8,7 +8,8 @@ from bika.lims.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from bika.lims import bikaMessageFactory as _
 from bika.lims.utils import t
-from bika.lims.utils import formatDateQuery, formatDateParms, formatDuration
+from bika.lims.utils import formatDateQuery, formatDateParms, formatDuration,\
+        formatPortalCatalogDateQuery
 from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 
@@ -189,13 +190,9 @@ class Report(BrowserView):
             writer.writerow([])
             date_query = formatDateQuery(self.context, 'Received')
             if date_query:
-                string_dates = []
-                for i in date_query['query']:
-                    string_dates.append(
-                            datetime.datetime.strptime(
-                                i, '%Y-%m-%d %H:%M').strftime('%Y-%m-%d'))
-                dates_requested = ' - '.join(string_dates)
-                writer.writerow(['Date Received', dates_requested])
+                dates_rec = formatPortalCatalogDateQuery(date_query['query'])
+                writer.writerow(
+                        ['Dates Received', dates_rec[0], dates_rec[1]])
             if 'getInstrumentUID' in self.request.form:
                 writer.writerow(['Instrument', instrument_title])
             if 'Analyst' in self.request.form:
